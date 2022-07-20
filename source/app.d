ae3e557530f58868550b45599fbb83be6497f219
Kyle Coffey <me@phytolizer.dev>
2022-07-20 13:40:21 -0500

    progress report

--- source/app.d
+++ source/app.d
@@ -3,14 +3,20 @@ import std.stdio;
 immutable int IMAGE_WIDTH = 256;
 immutable int IMAGE_HEIGHT = 256;
 
+immutable string PROGRESS_MSG = "Scanlines remaining: ";
+immutable int PROGRESS_MSG_SIZE = PROGRESS_MSG.length + 1;
+
 void main()
 {
 	writeln("P3");
 	writeln(IMAGE_WIDTH, ' ', IMAGE_HEIGHT);
 	writeln(255);
+	stderr.write(PROGRESS_MSG);
 
 	foreach_reverse (int j; 0 .. IMAGE_HEIGHT)
 	{
+		stderr.write("\x1b[?25l\x1b[", PROGRESS_MSG_SIZE, "G\x1b[K", j, "\x1b[?25h");
+		stderr.flush();
 		foreach (int i; 0 .. IMAGE_WIDTH)
 		{
 			auto r = cast(double)(i) / (IMAGE_WIDTH - 1);
@@ -24,4 +30,6 @@ void main()
 			writeln(ir, ' ', ig, ' ', ib);
 		}
 	}
+
+	stderr.writeln("\nDone.");
 }
