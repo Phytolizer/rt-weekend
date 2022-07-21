658dd1855f29d473fd8e5113b4f5c80855bc11eb
Kyle Coffey <me@phytolizer.dev>
2022-07-21 10:15:33 -0500

    sphere

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+#![allow(dead_code)]
+
 use std::io::{stderr, stdout, Write};
 
 use ray::Ray;
-use vec3::{Point3, Vec3};
+use vec3::{dot, Point3, Vec3};
 
 use crate::{color::write_color, vec3::Color};
 
@@ -21,7 +23,19 @@ const VERTICAL: Vec3 = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
 const PROGRESS_MESSAGE: &str = "Scanlines remaining: ";
 const PROGRESS_MSG_SIZE: usize = PROGRESS_MESSAGE.len() + 1;
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+    let oc = *r.origin() - *center;
+    let a = dot(r.direction(), r.direction());
+    let b = 2.0 * dot(&oc, r.direction());
+    let c = dot(&oc, &oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = r.direction().unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
